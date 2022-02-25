5024f9226b1961e7c583d4cd0d4ffb2d310bf29e
Tilian HURÉ <58342016+Tilian-HURE@users.noreply.github.com>
2022-02-25 12:13:25 +0100

    Add files via upload

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"test","l":"MainSchtroumpf"},{"p":"test","l":"MainVillage"},{"p":"lesschtroumpfs","l":"Schtroumpf"},{"p":"lesschtroumpfs","l":"Village"}]
